87c30c17e23ce122b91994dbfcda0be0c860f5a5
Irina-Komaritskaya <amsterdam.ira@yandex.ru>
2021-09-19 12:54:46 +0700

    loading by API

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
 import style from './app.module.css';
 import Header from '../header/header';
 import Main from '../main/main';
-import data from '../../utils/data'
 
 function App() {
+  const url = 'https://norma.nomoreparties.space/api/ingredients'
+
+  const [state, setState] = useState({
+    isLoading: false,
+    hasError: false,
+    data: []
+  });
+
+  useEffect(()=>{
+    const getData = async () => {
+      try{
+        setState({...state, hasError: false, isLoading: true})
+        const res = await fetch(url);
+        const json = await res.json();
+        setState({...state, data: json.data, isLoading: false});
+      }
+      catch(e){
+        setState({...state, hasError: true, isLoading: false})
+      }
+    } 
+    getData();
+  }, []);
+  
+  const { data, isLoading, hasError } = state;
+
 	return (
 		<div className={style.app}>
 			<Header />
-			<Main data={data}/>
+      
+			<Main data={data} isLoading={isLoading} hasError={hasError}/>
 		</div>
 	);
 }
